cb70ef5b23cbebe610c6163b732fcfbf4034c6b3
Muhammed Hedyia <hedyia@hotmal.com>
2018-12-01 09:02:56 +0200

    InitialCommit

---
+++ Services/IApiService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Khreje.Models;
+
+namespace Khreje.Services
+{
+    public interface IApiService
+    {
+        Task<Response> CheckConnection();
+        Task<Response> Delete<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, T model);
+        Task<Response> Get<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, int id);
+        Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller);
+        Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken);
+        Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, int id);
+        Task<TokenResponse> GetToken(string urlBase, string username, string password);
+        Task<Response> Post<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, T model);
+        Task<Response> Post<T>(string urlBase, string servicePrefix, string controller, T model);
+        Task<Response> Put<T>(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, T model);
+    }
+}
